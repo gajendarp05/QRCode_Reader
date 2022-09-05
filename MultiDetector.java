5f6061c29a836e3e28002be774029aa0ba9f9712
Gajendar Pandey <gajendarp05@gmail.com>
2022-09-05 12:29:54 +0530

    Create MultiDetector.java

---
+++ MultiDetector.java
@@ -0,0 +1,42 @@
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.DetectorResult;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.multi.qrcode.detector.MultiDetector;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class QRCodeMain {
+    private static void detectQRCode(File qrCodeimage) throws IOException {
+        // BufferImage handle and manipulate the image data
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        // HybridBinarizer: It is designed for high frequency images of barcodes with black data on white backgrounds
+        // Convert luminance data to 1 bit data.
+        // BinaryBitmap: to represent 1 bit data.
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            DetectorResult[] result = new MultiDetector(bitmap.getBlackMatrix()).detectMulti(null);
+            for (DetectorResult i: result) {
+                System.out.println(i.getBits());
+            }
+
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Reading from file and giving File file instance
+        try {
+            File file = new File("abc.jpg");
+            detectQRCode(file);
+        } catch (IOException e) {
+            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
